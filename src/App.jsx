113ddf02201bdc3ccede43cacd1183fe90e00415
pksingh <pksingh69313@gmail.com>
2025-08-14 15:59:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind Working! 🌤️
+      </h1>
+    </div>
+  )
+}
+
+export default App
