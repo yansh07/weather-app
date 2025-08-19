e2f91dca04b5d9e6b9ada93cae26c6f16de67228
pksingh <pksingh69313@gmail.com>
2025-08-19 18:35:36 +0530

    Added page title, and gradient

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
-        <h1 className='text-black bg-red-800'>Priyanshu Kumar Singh</h1>
+    <div className='p-8'>
+        <h1 className="text-5xl font-bold text-transparent font-heading bg-gradient-to-r from-amber-300 via-orange-500 to-pink-600 bg-clip-text">NIMBUS</h1>
     </div>
   )
 }
