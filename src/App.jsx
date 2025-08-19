a5e4679be9e09c3de20484cbbbf8a8ef5cf64707
pksingh <pksingh69313@gmail.com>
2025-08-19 18:05:30 +0530

    Initialised files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
 import './index.css'
+import Home from './Home';
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-5xl font-bold text-white">
-        Tailwind Working! 🌤️
-      </h1>
+    <div className='relative'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  )
+  );
 }
-
 export default App
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'YOUR_API_KEY_HERE',
+//     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+//   }
+// };
+
+// async function fetchCities(query) {
+//   const res = await fetch(
+//     `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${query}&limit=5&sort=-population`,
+//     options
+//   );
+//   const data = await res.json();
+//   return data.data; // array of cities
+// }
+
+// // Example usage
+// fetchCities("del").then(cities => console.log(cities));
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Components/Hero"
+
+function Home() {
+  return (
+    <div className='relative z-10 min-h-screen overflow-x-hidden'>
+      <Hero />
+    </div>
+  )
+}
+
+export default Home
