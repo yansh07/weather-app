1268eae6d174cb772084a84a2b21c642e47490aa
pksingh <pksingh69313@gmail.com>
2025-08-20 14:49:55 +0530

    Added hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div className='p-8'>
-        <h1 className="text-5xl font-bold text-transparent font-heading bg-gradient-to-r from-amber-300 via-orange-500 to-pink-600 bg-clip-text">NIMBUS</h1>
+    <div className="flex flex-row gap-16 p-8 mb-4">
+      <h1 className="text-5xl font-bold text-transparent font-heading bg-gradient-to-r from-green-500 via-slate-600 to-white/70 bg-clip-text">
+        NIMBUS
+      </h1>
+
+      <div className="object-cover pl-2 pr-2 card">
+        <i class="fa-solid fa-magnifying-glass-location text-white/70 text-3xl mt-1"></i>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "./Components/Hero";
 
 function Home() {
   return (
-    <div className='relative z-10 min-h-screen overflow-x-hidden'>
+    <div className='relative z-10 min-h-screen overflow-x-hidden bg-glassy'>
       <Hero />
     </div>
   )
