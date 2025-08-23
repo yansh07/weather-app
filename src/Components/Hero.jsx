b43ece438565dc45f0484b06097f7220bdb39c76
pksingh <pksingh69313@gmail.com>
2025-08-23 15:30:57 +0530

    FIXED: Resonsiveness on sm screens

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
   const now = dayjs();
 
   return (
-    <div className="flex flex-row items-center gap-12 p-2 px-8 md:gap-40 lg:px-36 xl:gap-96 xl:ml-10">
+    <div className="flex flex-row items-center gap-10 p-2 px-8 md:gap-36 md:px-2 lg:px-32 xl:gap-96 xl:ml-10 lg:gap-44">
 
-      <h1 className="text-lg font-bold text-transparent font-heading md:text-2xl xl:text-4xl md:px-20 md:pt-4 bg-gradient-to-r from-orange-400 via-yellow-400 to-white/70 bg-clip-text">
+      <h1 className="text-lg font-bold text-transparent font-heading md:text-2xl xl:text-4xl md:px-24 md:pt-4 bg-gradient-to-r from-orange-400 via-yellow-400 to-white/70 bg-clip-text">
         NIMBUS
       </h1>
-      <div className="flex gap-3 px-2 text-xs md:text-sm md:pt-4 md:px-6 xl:text-lg xl:px-94 text-gray-300/90 font-heading">
+      <div className="flex gap-3 px-2 text-xs md:text-sm md:pt-4 xl:text-lg xl:px-92 xl:px-8 text-gray-300/90 font-heading">
         <div>{now.format("dddd")}</div>
         <div>{now.format("DD MMM, YYYY  h:mm A")}</div>
       </div>
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -18,7 +18,7 @@ export default function WeatherSearch({ onSearch }) {
         value={city}
         onChange={(e) => setCity(e.target.value)}
         onKeyDown={handleKeyDown}
-        className="p-2 border border-gray-300 rounded-lg shadow-sm w-80 md:w-96 lg:w-[580px] xl:w-[900px] focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-200/90 placeholder:text-black"
+        className="text-black font-semibold p-2 border border-gray-300 rounded-lg shadow-sm w-80 md:w-[550px] lg:w-[580px] xl:w-[900px] focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-200/90 placeholder:text-black"
       />
     </div>
   );
