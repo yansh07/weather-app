6c724fb34fb04ec10f3ccc5c5b4e3b726560e5ed
pksingh <pksingh69313@gmail.com>
2025-08-23 19:42:37 +0530

    FIXED: Fixed responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   const now = dayjs();
 
   return (
-    <div className="flex flex-row items-center gap-24 p-2 px-4 mt-4 md:gap-32 md:px-8 lg:px-32 xl:gap-44 xl:ml-20 xl:px-96 lg:gap-48">
+    <div className="flex flex-row items-center gap-24 p-2 px-4 mt-4 md:gap-32 md:px-8 lg:px-32 xl:gap-44 xl:ml-40 xl:px-92 lg:gap-48">
 
       <h1 className="text-lg font-bold text-transparent font-heading md:text-2xl xl:text-4xl md:px-24 md:pt-4 bg-gradient-to-r from-orange-400 via-yellow-400 to-white/70 bg-clip-text">
         NIMBUS
